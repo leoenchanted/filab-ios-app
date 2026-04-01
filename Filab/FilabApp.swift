4799c1d6aa11e4fecae873611d39ce5ae4134cde
Leo <1198067041@qq.com>
2026-04-02 01:01:16 +0800

    Initial Commit

---
+++ Filab/FilabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilabApp.swift
+//  Filab
+//
+//  Created by leo on 2026/4/2.
+//
+
+import SwiftUI
+
+@main
+struct FilabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FilabTests/FilabTests.swift
@@ -0,0 +1,19 @@
+//
+//  FilabTests.swift
+//  FilabTests
+//
+//  Created by leo on 2026/4/2.
+//
+
+import Testing
+@testable import Filab
+
+struct FilabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
